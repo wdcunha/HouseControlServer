ab77287184954443a6a9dfce464a3404e168c4bf
wdcunha@gmail.com <wdcunha@gmail.com>
2019-06-25 00:21:36 +0100

    implements Firebase Cloud Messaging funcionality, tested and running

---
+++ src/main/java/pt/ipp/estg/housecontrol/ServerStarter.java
@@ -0,0 +1,83 @@
+package pt.ipp.estg.housecontrol;
+
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+public class ServerStarter {
+
+    private static String[] SCOPES = { "https://www.googleapis.com/auth/firebase.messaging"};
+
+
+    public static void main(String[] args) throws IOException, JSONException {
+
+        sendFCMNewData();
+
+    }
+
+    private static void sendFCMNewData() throws IOException, JSONException {
+        URL url = new URL("https://fcm.googleapis.com/v1/projects/housecontrolmobile/messages:send");
+
+        String token = getAccessToken();
+        String tokenApp = "cuh2hmdJDuw:APA91bHWSn_8izzRva2GFtRQCSzdFOMMDDh8c8KrNml3_3IQ7jWaaqq8nZ8EpSy0o4TKYMJnt8aw2P2myqCQeScjW9_fHeWzRqNqDcvwVQ9ciRQyDIdohoqtlJlPH-tO6DvgErGSsc8A";
+        String title = "sixth message from HCServer";
+        String message = "Notificação FCM enviada pelo HCServer!";
+        String body = "{ \"message\": {\"token\": \""+tokenApp+"\",\"notification\" : {\"title\" : \""+title+"\",\"body\" : \""+message+"\"}}}";
+        System.out.println("token: "+token);
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+//        httpURLConnection.setConnectTimeout(10000);
+        httpURLConnection.setRequestProperty("Authorization", "Bearer " + token);
+        httpURLConnection.setRequestProperty("Content-Type", "application/json; UTF-8");
+
+        OutputStream outputStream = httpURLConnection.getOutputStream();
+        outputStream.write(body.getBytes());
+
+        outputStream.flush();
+
+        StringBuilder sb = new StringBuilder();
+
+        int httpResult = httpURLConnection.getResponseCode();
+
+        if(httpResult == HttpURLConnection.HTTP_OK){
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(httpURLConnection.getInputStream(),"utf-8")
+            );
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null){
+                sb.append(line + "\n");
+            }
+            System.out.println("Message sent succesfully!!!!");
+            bufferedReader.close();
+            httpURLConnection.disconnect();
+            //return httpURLConnection.toString();
+
+        } else {
+            httpURLConnection.disconnect();
+            //return httpURLConnection.getResponseMessage();
+        }
+    }
+
+    private static String getAccessToken() throws IOException {
+        GoogleCredential myGoogleCredential = GoogleCredential
+                .fromStream(new FileInputStream("/Users/wdcunha/ESTG/2osemestre/Des Web/TrabalhoFinal/HouseControlServer/src/main/java/resources/housecontrolmobile-firebase-adminsdk-qv0hl-b0b6bf3ff5.json"))
+                .createScoped(Arrays.asList(SCOPES));
+        myGoogleCredential.refreshToken();
+        return myGoogleCredential.getAccessToken();
+    }
+
+}
